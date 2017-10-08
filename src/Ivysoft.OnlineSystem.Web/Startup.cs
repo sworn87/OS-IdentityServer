97baea2666efe7232544bc668e1d76487ffc0519
Ivy-soft <ivy-soft@abv.bg>
2017-10-08 23:21:48 +0300

    Added IdentityServer4.EntityFramework

--- src/Ivysoft.OnlineSystem.Web/Startup.cs
+++ src/Ivysoft.OnlineSystem.Web/Startup.cs
@@ -10,11 +10,17 @@ using Ivysoft.OnlineSystem.Web.Infrastructure.IdentityServer;
 using Ivysoft.OnlineSystem.Data;
 using Ivysoft.OnlineSystem.Data.Models;
 using Ivysoft.OnlineSystem.Data.Migrations;
+using IdentityServer4.EntityFramework.Mappers;
+using IdentityServer4.EntityFramework.DbContexts;
+using System.Linq;
+using System.Reflection;
 
 namespace Ivysoft.OnlineSystem.Web
 {
     public class Startup
     {
+        private const string connectionStringName = "DefaultConnection";
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -25,8 +31,10 @@ namespace Ivysoft.OnlineSystem.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<OnlineSystemDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            
+
+        services.AddDbContext<OnlineSystemDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString(connectionStringName)));
 
             services.AddTransient<OnlineSystemSeedData>();
 
@@ -39,14 +47,35 @@ namespace Ivysoft.OnlineSystem.Web
 
             services.AddMvc();
 
+            string connectionString = Configuration.GetConnectionString(connectionStringName);
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
             // configure identity server with in-memory stores, keys, clients and scopes
             services.AddIdentityServer()
                 .AddDeveloperSigningCredential()
-                .AddInMemoryPersistedGrants()
-                .AddInMemoryIdentityResources(Config.GetIdentityResources())
-                .AddInMemoryApiResources(Config.GetApiResources())
-                .AddInMemoryClients(Config.GetClients())
-                .AddAspNetIdentity<User>();
+                .AddAspNetIdentity<User>()
+                .AddConfigurationStore(options =>
+                {
+                    options.ConfigureDbContext = builder =>
+                        builder.UseSqlServer(connectionString,
+                            sql => sql.MigrationsAssembly(migrationsAssembly));
+                })
+                // this adds the operational data from DB (codes, tokens, consents)
+                .AddOperationalStore(options =>
+                {
+                    options.ConfigureDbContext = builder =>
+                        builder.UseSqlServer(connectionString,
+                            sql => sql.MigrationsAssembly(migrationsAssembly));
+
+                    // this enables automatic token cleanup. this is optional.
+                    options.EnableTokenCleanup = true;
+                    options.TokenCleanupInterval = 30;
+                });
+                // this adds the config data from DB (clients, resources)
+                //.AddInMemoryPersistedGrants()
+                //.AddInMemoryIdentityResources(Config.GetIdentityResources())
+                //.AddInMemoryApiResources(Config.GetApiResources())
+                //.AddInMemoryClients(Config.GetClients())
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -85,6 +114,46 @@ namespace Ivysoft.OnlineSystem.Web
             {
 
                 throw ex;
+            }
+
+            this.InitializeDatabase(app);
+        }
+
+        private void InitializeDatabase(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                serviceScope.ServiceProvider.GetRequiredService<OnlineSystemDbContext>().Database.Migrate();
+                serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+                var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+                context.Database.Migrate();
+                if (!context.Clients.Any())
+                {
+                    foreach (var client in Config.GetClients())
+                    {
+                        context.Clients.Add(client.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.IdentityResources.Any())
+                {
+                    foreach (var resource in Config.GetIdentityResources())
+                    {
+                        context.IdentityResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.ApiResources.Any())
+                {
+                    foreach (var resource in Config.GetApiResources())
+                    {
+                        context.ApiResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
             }
         }
     }
